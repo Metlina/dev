1ff0b5c931d3984ea94947e1affb46a930f7eab5
Tino Petrina <tinopetrina@live.com>
2015-03-12 23:16:32 +0100

    added new layout

--- Xamarin/Android/FirstApp/FirstApp/MainActivity.cs
+++ Xamarin/Android/FirstApp/FirstApp/MainActivity.cs
@@ -11,7 +11,7 @@ namespace FirstApp
     [Activity(Label = "FirstApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        //int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,14 +22,20 @@ namespace FirstApp
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            //Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate
-            {
-                button.Text = string.Format("{0} clicks!", count++);
-            };
+            //button.Click += delegate
+            //{
+            //    button.Text = string.Format("{0} clicks!", count++);
+            //};
 
-            EditText et = FindViewById<EditText>(Resource.Id.editText1);
+            //EditText et = FindViewById<EditText>(Resource.Id.editText1);
+
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            EditText phoneNumberEditText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
         }
     }
 }
