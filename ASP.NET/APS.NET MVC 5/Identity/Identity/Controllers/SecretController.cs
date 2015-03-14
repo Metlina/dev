46be874a3f3b3fe2b0b49ca2113e694f9d55bc59
Tino Petrina <tinopetrina@live.com>
2015-03-14 15:58:21 +0100

    aspnet identity project

---
+++ ASP.NET/APS.NET MVC 5/Identity/Identity/Controllers/SecretController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This is not a secret...");
+        }
+    }
+}
