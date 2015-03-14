ca5a0e01ca9637a403b56090c35e44570feaaf68
Tino Petrina <tinopetrina@live.com>
2015-03-14 14:20:40 +0100

    owinKatana deleted

--- Xamarin/Android/FirstApp/FirstApp/MainActivity.cs
+++ Xamarin/Android/FirstApp/FirstApp/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Views.InputMethods;
 
 namespace FirstApp
 {
@@ -31,11 +32,34 @@ namespace FirstApp
 
             //EditText et = FindViewById<EditText>(Resource.Id.editText1);
 
-            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
 
-            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+            var callButton = FindViewById<Button>(Resource.Id.CallButton);
 
-            EditText phoneNumberEditText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var phoneNumberEditText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+
+            translateButton.Click += delegate
+            {
+                //keyboard dismissal
+                var imm = (InputMethodManager) GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(phoneNumberEditText.WindowToken, 0);
+            };
+
+            callButton.Click += delegate
+            {
+
+            };
+
+            //var dialog = new AlertDialog.Builder(this);
+
+            //dialog.SetTitle("Title");
+            //dialog.SetMessage("Message Goes Here");
+
+            //var callIntent = new Intent(Intent.ActionCall);
+
+            //callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+
+            //StartActivity(callIntent);
         }
     }
 }
