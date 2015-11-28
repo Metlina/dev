c9f2d2114ef21b5dc6510bc7da8cdec39c93e97c
Tino Petrina <tinopetrina@live.com>
2015-11-28 09:19:31 +0100

    priprema 4 labos aspdotnet

---
+++ CetvrtiLabos/SlideShow.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="SlideShow.aspx.cs" Inherits="SlideShow" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div id="body">
+        <asp:ScriptManager ID="SriptManager1" runat="server">
+        </asp:ScriptManager>
+        
+        <asp:UpdatePanel runat="server">
+            <ContentTemplate>
+                <asp:Image ID="Image1" ImageUrl="/Images/Image1.jpg" runat="server"/>
+                <asp:Timer ID="Timer1" Interval="1000" OnTick="Timer1_OnTick" runat="server"/>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+</asp:Content>
+
---
+++ CetvrtiLabos/SlideShow.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public partial class SlideShow : System.Web.UI.Page
+{
+    private int picNumber = 8;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (User.Identity.IsAuthenticated == false)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+
+    protected void Timer1_OnTick(object sender, EventArgs e)
+    {
+        int count;
+        if (ViewState["PicCounter"] == null)
+        {
+            count = 2;
+        }
+        else if (Convert.ToInt32(ViewState["PicCounter"]) > picNumber)
+        {
+            count = 1;
+        }
+        else
+        {
+            count = Convert.ToInt32(ViewState["PicCounter"]);
+        }
+
+        Image1.ImageUrl = "/Images/Image" + (count++) + ".jpg";
+        ViewState["PicCounter"] = count;
+    }
+}
