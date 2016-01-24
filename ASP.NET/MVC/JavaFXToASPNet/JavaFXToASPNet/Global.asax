9001a3e20252d24c0d464190ce62643285e23d3e
Tino Petrina <tinopetrina@live.com>
2016-01-24 20:04:55 +0100

    added solution for projekt for asp net forms

--- ASP.NET/MVC/JavaFXToASPNet/JavaFXToASPNet/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JavaFXToASPNet.MvcApplication" Language="C#" %>
---
+++ ASP.NET/WebForms/Printer/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Printer.Startup))]
+namespace Printer
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
